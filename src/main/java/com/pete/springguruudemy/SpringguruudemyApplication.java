9a30df9a3e7f900bafac1dd76e35b28198554ad6
Petea <11.fenix.96@gmail.com>
2020-08-26 15:17:18 +0300

    Initial Commit

---
+++ src/main/java/com/pete/springguruudemy/SpringguruudemyApplication.java
@@ -0,0 +1,13 @@
+package com.pete.springguruudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringguruudemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringguruudemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pete/springguruudemy/SpringguruudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pete.springguruudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringguruudemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
